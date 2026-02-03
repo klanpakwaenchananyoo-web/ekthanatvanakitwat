3a689b9290d6924a5c708b86dcbbff22b57c4f7b
klanpakwaenchananyoo <klanpakwaenchananyoo@gmail.com>
2026-02-03 15:16:00 +0700

    lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,37 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *ptr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(ptr + i);
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double *ptr, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(ptr + i) = (rand() % 101) / 100.0;
+    }
+}
+
+void findRowSum(const double* matrix, double* result, int m, int n) {
+    for (int i = 0; i < m; ++i) {
+        double currentSum = 0;
+        for (int j = 0; j < n; ++j) {
+            currentSum += *(matrix + (i * n + j));
+        }
+        result[i] = currentSum;
+    }
+}
+
+void findColSum(const double* matrix, double* result, int m, int n) {
+    for (int j = 0; j < n; ++j) {
+        result[j] = 0;
+    }
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            result[j] += *(matrix + (i * n + j));
+        }
+    }
+}
